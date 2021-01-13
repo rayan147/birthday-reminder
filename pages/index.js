f5fd0634b1056870526a3fea1c9e2ec6a31e52de
Rayan Ramirez <rayan361@gmail.com>
2021-01-12 19:01:23 -0500

    initail commit

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>CryptoBoard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
        <main>
